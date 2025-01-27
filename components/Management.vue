7c1ead99cdf2d20a2d7ac75937027221dfecb006
Ditaaa <113565619+ditanursabila@users.noreply.github.com>
2025-01-27 14:34:02 +0700

    responsive content

--- components/Management.vue
+++ components/Management.vue
@@ -11,29 +11,28 @@
           experiences in music licensing.
         </h2>
 
-        <div class="flex justify-center">
-          <div class="grid grid-cols-2 gap-10 mt-10 mb-10">
+        <div class="flex justify-center px-4 sm:px-6 lg:px-8">
+          <div
+            class="grid grid-cols-1 sm:grid-cols-2 gap-6 sm:gap-8 md:gap-10 mt-6 sm:mt-8 md:mt-10 mb-6 sm:mb-8 md:mb-10">
             <div class="text-center" data-aos="flip-left" duration="3000">
-              <img class="rounded-full border border-slate-500 shadow-lg w-48 h-48 mx-auto" src="assets/img/Jusak.jpg"
-                alt="Jusak" />
+              <img
+                class="rounded-full border border-slate-500 shadow-lg w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 mx-auto"
+                src="assets/img/Jusak.jpg" alt="Jusak" />
               <div class="mt-2 sm:mt-4">
-                <h3 class="font-medium text-xl text-gray-800">Jusak Setiono</h3>
-                <p class="text-md text-gray-600">Director</p>
+                <h3 class="font-medium text-lg sm:text-xl text-gray-800">Jusak Setiono</h3>
+                <p class="text-sm sm:text-md text-gray-600">Director</p>
               </div>
             </div>
-            <!-- End Col -->
 
             <div class="text-center" data-aos="flip-right" duration="3000">
-              <img class="rounded-full border border-slate-500 shadow-lg w-48 h-48 mx-auto"
+              <img
+                class="rounded-full border border-slate-500 shadow-lg w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 mx-auto"
                 src="assets/img/salfitri.jpg" alt="Salfitri" />
               <div class="mt-2 sm:mt-4">
-                <h3 class="font-medium text-xl text-gray-800">
-                  Salfitri Tri Dasa Yunita
-                </h3>
-                <p class="text-md text-gray-600">Financial</p>
+                <h3 class="font-medium text-lg sm:text-xl text-gray-800">Salfitri Tri Dasa Yunita</h3>
+                <p class="text-sm sm:text-md text-gray-600">Financial</p>
               </div>
             </div>
-            <!-- End Col -->
           </div>
         </div>
       </div>
