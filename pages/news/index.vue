03ae2d41ec5538b37d325f100740e18f6ac9991b
tejar <tejarm04@gmail.com>
2025-01-31 14:07:41 +0700

    add translate

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -17,6 +17,7 @@ import news3 from '@/assets/img/news3.jpg';
 
 const loading = ref(true);
 
+
 onMounted(() => {
   setTimeout(() => {
     loading.value = false;
@@ -77,12 +78,12 @@ const berita = [
     </div>
   </div>
   <!-- End Title -->
-  <!-- Card Blog -->
+    <!-- Card Blog -->
   <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
     <!-- Grid -->
     <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
-      <!-- Card -->
-      <a v-for="news in berita" :key="news.id" class="group flex flex-col focus:outline-none border-2 border-slate-200 rounded-2xl p-4 hover:border-yellow-400 transition-all duration-300 hover:shadow-lg" href="/news/detail">
+   <!-- Card -->
+   <a v-for="news in berita" :key="news.id" class="group flex flex-col focus:outline-none border-2 border-slate-200 rounded-2xl p-4 hover:border-yellow-400 transition-all duration-300 hover:shadow-lg" :href="`/news/${news.id}`">
         <div class="relative pt-[50%] sm:pt-[70%] rounded-xl overflow-hidden">
           <img class="size-full absolute top-0 start-0 object-cover group-hover:scale-105 group-focus:scale-105 transition-transform duration-500 ease-in-out rounded-xl" :src="news.image" :alt="news.alt" />
         </div>
@@ -96,7 +97,7 @@ const berita = [
               <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="m9 18 6-6-6-6" />
               </svg>
-              </p>
+            </p>
           </div>
         </div>
       </a>
