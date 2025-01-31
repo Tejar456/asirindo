8714f6370f17e43f4258d0935511cc917d7ab29e
tejar <tejarm04@gmail.com>
2025-01-31 11:37:22 +0700

    first commit

---
+++ i18n.config.ts
@@ -0,0 +1,131 @@
+import type Solution from "./components/Solution.vue";
+
+export default defineI18nConfig(() => ({
+  fallbackLocale: "id",
+  messages: {
+    id: {
+      // navbar
+      home: "Beranda",
+      about: "Tentang",
+      solution: "Solusi",
+      content: "Konten",
+      contact: "Kontak",
+      management: "Pengelola",
+      catalog: "Katalog",
+      news: "Berita",
+      //   hero
+      hero: "Musik adalah alat yang efektif untuk melibatkan pelanggan Anda",
+      hero2: "Anda membutuhkan musik di restoran, karaoke, dan hotel Anda",
+      hero3: "Musik adalah bagian integral dalam bisnis perhotelan",
+      hero4: "Bisnis penyiaran perlu memiliki produk yang legal",
+      hero5:
+        "Dukung musik Indonesia dengan lagu-lagu yang terlisensi secara legal",
+      Previous: "Sebelumnya",
+      button: "Selanjutnya",
+      text: "ASIRINDO adalah solusi lisensi musik satu atap untuk pelanggan bisnis di Indonesia.",
+      Contact: "Hubungi Kami",
+      More: "Pelajari Lebih Lanjut",
+      talk: "Butuh lisensi musik? Bicaralah kepada kami!",
+      talk1:
+        "ASIRINDO telah melisensikan ribuan lagu Indonesia untuk digunakan secara legal di restoran, kafe, hotel, karaoke, siaran, dan bisnis lainnya, sehingga Anda tidak perlu khawatir.",
+      talkContact: "Hubungi Kami",
+      talkMore: "Pelajari Lebih Lanjut",
+      //   about
+      title: "Tentang Asirindo",
+      desk: "ASIRINDO adalah pusat musik pertama dan satu-satunya di Indonesia. Kami telah melisensikan ribuan lagu dari ratusan produser dan label sehingga Anda tidak perlu melakukannya. Musik membantu melibatkan pelanggan dengan produk dan layanan Anda. Kami di sini untuk membantu Anda mencapai itu.",
+      card1: "Konten 100% Legal",
+      descard1:
+        "Kami melisensikan musik dari pemilik hak. Kami membantu Anda menjawab tantangan untuk menjadi 100% legal.",
+      card2: "Dukungan Profesional",
+      descard2:
+        "Tim kami adalah para profesional paling berpengalaman di bidang lisensi musik dan siap mendukung semua klien kami.",
+      card3: "Katalog Kaya",
+      descard3:
+        "Kami memiliki katalog musik Indonesia yang paling lengkap yang siap digunakan untuk membantu Anda meningkatkan penawaran bisnis Anda.",
+      card4: "Satu Sumber untuk Semua",
+      descard4:
+        "Anda hanya perlu berurusan dengan ASIRINDO untuk menjadi 100% legal, mendapatkan dukungan profesional, dan katalog musik Indonesia yang paling lengkap.",
+      // solution
+      titlesolution: "Aset Konten Kami",
+      desksolution:
+        "Semua yang kami miliki telah terlisensi secara legal dan siap digunakan.",
+      vidio: "VIDEO",
+      audio: "AUDIO",
+      database: "DATABASE",
+      producers: "PRODUSER",
+      //   management
+      titlemanagement: "Manajemen",
+      deskmanagement:
+        "ASIRINDO dijaga dengan aman oleh eksekutif musik dengan pengalaman puluhan tahun di bidang lisensi musik.",
+      Director: "Direktur",
+      Financial: "Keuangan",
+      //   kontak
+      titlecontact: "Hubungi Kami",
+      Submit: "Kirim",
+      //   news
+      titlenews: "Berita",
+      Read: "Baca Selengkapnya",
+    },
+    en: {
+      // navbar
+      home: "Home",
+      about: "About",
+      solution: "Solution",
+      content: "Content",
+      management: "Management",
+      catalog: "Catalog",
+      contact: "Contact",
+      news: "News",
+      // hero
+      hero: "Music is an effective tool to engage your customers",
+      hero2: "You need music in your restaurants karaoke and hotels",
+      hero3: "Music is an integral part in hospitality business",
+      hero4: "Broadcasting businesses need to have legal products",
+      hero5: "Support Indonesian music with legally licensed songs",
+      Previous: "Previous",
+      button: "Next",
+      text: " ASIRINDO is a one stop music licensing solution for business customers in Indonesia.",
+      Contact: "Contact Us",
+      More: "Learn More",
+      talk: "Need music license? Talk to us!",
+      talk1:
+        " ASIRINDO had licensed thousands of Indonesian songs to use legally in restaurants, cafes, hotels, karaoke, broadcasts and any other businesses, so you don't have to",
+      talkContact: "Contact Us",
+      talkMore: "Learn More",
+      //   about
+      title: "About Asirindo",
+      desk: "ASIRINDO is the first and the only music hub in Indonesia. We've licensed thousands of songs from hundreds of producers and labels so you don't have to. Music helps engage customers with your products and services. We're here to help you achieve that.",
+      card1: "100% Legal Content",
+      descard1:
+        "We license music from rights owner. We help you answer the challenge to go 100% legal.",
+      card2: "Professional Support",
+      descard2:
+        "Our team is the most experienced professionals in the music licensing field and ready to support all our clients.",
+      card3: "Rich Catalog",
+      descard3:
+        "We have the most complete Indonesian music catalog that is ready to use to help you enhance your business offers.",
+      card4: "One Resource for All",
+      descard4:
+        "You only need to deal with ASIRINDO to go 100% legal, get professional support and the most complete Indonesian music catalog.",
+      // solution
+      titlesolution: "Our Content Assets",
+      desksolution: "Everything we have are legally licensed and ready to use.",
+      vidio: "VIDEO",
+      audio: "AUDIO",
+      database: "DATABASE",
+      producers: "PRODUCERS",
+      //   management
+      titlemanagement: "Management",
+      deskmanagement:
+        "ASIRINDO is safely guarded by music executives with decades of experiences in music licensing.",
+      Director: "Director",
+      Financial: "Financial",
+      //   contact
+      titlecontact: "Contact Us",
+      Submit: "Submit",
+      //   news
+      titlenews: "News",
+      Read: "Read More",
+    },
+  },
+}));
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,26 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  modules: [
+    '@nuxtjs/i18n',
+  ],
+  i18n: {
+    locales: ['en', 'id'],
+    defaultLocale: 'id',
+    detectBrowserLanguage: false,
+    strategy: 'no_prefix',  
+    vueI18n: './i18n.config.ts'
+  },
+  // i18n: {
+  //   locales: [
+  //     { code: 'id', iso: 'id-ID', name: 'Indonesia', flag: '🇮🇩' },
+  //     { code: 'en', iso: 'en-US', name: 'English', flag: '🇺🇸' }
+  //   ],
+  //   defaultLocale: 'id',
+  //   strategy: 'prefix_and_default', 
+  //   vueI18n: './i18n.config.ts'
+  // },
+
   plugins: ["~/plugins/preline.client.ts",
     { src: '~/plugins/aos.ts', mode: 'client', ssr: false }
   ],
