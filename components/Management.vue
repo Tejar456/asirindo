fb3dee28814d5eeb424ddf16c495655f8eb899d8
Ditaaa <113565619+ditanursabila@users.noreply.github.com>
2025-02-10 09:22:43 +0700

    update footer and foto

--- components/Management.vue
+++ components/Management.vue
@@ -1,8 +1,8 @@
 <template>
-  <section id="management items-center md:h-[90vh] ">
-    <div class="h-full flex ">
+  <section id="management">
+    <div class="h-full flex items-center md:h-[90vh]">
       <div class="mx-auto px-4">
-        <h1 class="text-center text-2xl md:text-4xl md:pt-2 font-semibold" data-aos="fade-up">
+        <h1 class="text-center text-2xl md:text-4xl font-semibold" data-aos="fade-up">
           {{ $t("titlemanagement") }}
         </h1>
         <div class="mx-auto h-1 w-20 md:w-28 bg-yellow-400 rounded mb-3 md:mb-4 mt-3 md:mt-4" data-aos="fade-up"></div>
@@ -25,8 +25,8 @@
 
             <div class="text-center" data-aos="flip-right" duration="3000">
               <img
-                class="rounded-full border border-slate-500 shadow-lg w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 mx-auto"
-                src="assets/img/Portrait_Placeholder.png" alt="Salfitri" />
+                class="rounded-full border border-slate-500 shadow-lg w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 mx-auto object-cover"
+                src="assets/img/salfitri2.jpg" alt="Salfitri" />
               <div class="mt-2 sm:mt-4">
                 <h3 class="font-medium text-lg sm:text-xl text-gray-800">Salfitri Tri Dasa Yunita</h3>
                 <p class="text-sm sm:text-md text-gray-600">{{ $t("financial") }}</p>
@@ -36,7 +36,7 @@
             <div class="text-center" data-aos="flip-right" duration="3000">
               <img
                 class="rounded-full border border-slate-500 shadow-lg w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 mx-auto"
-                src="assets/img/Portrait_Placeholder.png" alt="Patricia" />
+                src="assets/img/Portrait_Placeholder.png" alt="wiliam" />
               <div class="mt-2 sm:mt-4">
                 <h3 class="font-medium text-lg sm:text-xl text-gray-800">William</h3>
                 <p class="text-sm sm:text-md text-gray-600">{{ $t("direktur") }}</p>
@@ -46,7 +46,7 @@
             <div class="text-center" data-aos="flip-right" duration="3000">
               <img
                 class="rounded-full border border-slate-500 shadow-lg w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 mx-auto"
-                src="assets/img/Portrait_Placeholder.png" alt="Yasmine" />
+                src="assets/img/Portrait_Placeholder.png" alt="Yonathan" />
               <div class="mt-2 sm:mt-4">
                 <h3 class="font-medium text-lg sm:text-xl text-gray-800">Yonathan</h3>
                 <p class="text-sm sm:text-md text-gray-600">{{ $t("komisaris") }}</p>
