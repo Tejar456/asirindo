2c38c36d1aaac3b2b47c5e30de334ebf41a32886
tejar <tejarm04@gmail.com>
2025-02-18 14:29:13 +0700

    update licensing

--- components/Licensing.vue
+++ components/Licensing.vue
@@ -2,7 +2,7 @@
     <section id="lisencing">
     <!-- Solution -->
     <div
-      class="max-w-7xl px-4 py-10 sm:px-6 lg:px-8 lg:py-20 mx-auto h-full md:h-[90vh] mb-10"
+      class="max-w-7xl px-4 py-10 sm:px-6 lg:px-8 lg:py-20 mx-auto h-full md:h-full mb-10"
     >
       <header class="text-center">
         <h1
